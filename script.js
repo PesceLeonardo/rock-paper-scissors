884591b8f09a998e404062379aa0b819e732f5fb
Leonardo Pesce <196577953+PesceLeonardo@users.noreply.github.com>
2025-02-10 12:30:10 +0100

    Add and test getHumanChoice function With it the user can choose an option and proceed only if it's valid

--- script.js
+++ script.js
@@ -20,3 +20,24 @@ function getComputerChoice() {
 
 console.log(getComputerChoice());
 
+function getHumanChoice() {
+  while (true) {
+
+    try {
+      const properInputs = ["Rock", "Paper", "Scissors"];
+      const userInput = prompt("Choose your move! Rock, Paper, or Scissors?");
+
+      if (!properInputs.includes(userInput)) {
+        throw TypeError;
+
+      } else {
+        return userInput;
+      }
+      
+    } catch {
+      alert("Not a valid move! Try again, be sure to spell it correctly.")
+    }
+  }
+}
+
+console.log(getHumanChoice());

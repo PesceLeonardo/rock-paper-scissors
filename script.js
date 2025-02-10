c70489c3fba140b779ba8a7011b0f51d2c0f6da4
Leonardo Pesce <196577953+PesceLeonardo@users.noreply.github.com>
2025-02-10 15:22:36 +0100

    Add main logic Now an outcome will be generated for each round for games=5 rounds

--- script.js
+++ script.js
@@ -6,36 +6,42 @@ function capitalize(str) {
 
 
 
+// Useful Variables
+
+const properInputs = ["rock", "paper", "scissors"];
+
+const possibleMatches = {
+  rock: {
+    paper: "computer-win",
+    scissors: "human-win"
+  },
+  paper: {
+    rock: "human-win",
+    scissors: "computer-win"
+  },
+  scissors: {
+    rock: "computer-win",
+    paper: "human-win"
+  }
+};
+
+
+
 // Input Functions
 
-function getComputerChoice() {
+function getComputerChoice(inputs=properInputs) {
   const dice = Math.trunc(Math.random() * 3);
-  let move;
 
-  switch (dice) {
-    case 0:
-      move = "Rock";
-      break;
-
-    case 1:
-      move = "Paper";
-      break;
-
-    case 2:
-      move = "Scissors";
-      break;
-  }    
-  return move;
+  return inputs[dice];
 }  
 
-function getHumanChoice() {
+function getHumanChoice(inputs=properInputs) {
   while (true) {
-
+  
     try {
-      const properInputs = ["Rock", "Paper", "Scissors"];
-      const userInput = capitalize(prompt("Choose your move! Rock, Paper, or Scissors?").trim());
+      const userInput = prompt("Choose your move! Rock, Paper, or Scissors?").trim().toLowerCase();
 
-      if (!properInputs.includes(userInput)) {
+      if (!inputs.includes(userInput)) {
         throw TypeError;
 
       } else {
@@ -47,4 +53,51 @@ function getHumanChoice() {
     }  
   }  
 }  
+
+
+
+// Logic Functions
+
+function playRound(humanChoice, computerChoice, matches=possibleMatches) {
+  if (humanChoice === computerChoice) {
+    return "draw";
+  } else {
+    return matches[humanChoice][computerChoice];
+  }
+}
+
+function playGame(games=5) {
+  let humanScore = 0;
+  let computerScore = 0;
+
+  for (let round = 1; round < games + 1; round++) {
+    const humanChoice = getHumanChoice();
+    const computerChoice = getComputerChoice();
+    const result = playRound(humanChoice, computerChoice);
+
+    console.log(`The computer has selected "${capitalize(computerChoice)}"`);
+    console.log(`You have selected "${capitalize(humanChoice)}"`);
+
+    let message;
+    switch (result) {
+      case "draw":
+        message = "It's a draw!";
+        break;
+
+      case "human-win":
+        message = "You win!";
+        humanScore++;
+        break;
+
+      case "computer-win":
+        message = "The computer wins...";
+        computerScore++;
+        break;
+    }
+    console.log(message);
+    console.log(`Current score: computer ${computerScore} | you ${humanScore}`);
+  }
+}
+
+playGame();
 
